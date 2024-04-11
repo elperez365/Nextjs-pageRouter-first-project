019c2cb750b83e3734e3eb7d807124622a14ec31
vasco ceti <vasco.ceti@gmail.com>
2024-04-11 10:19:39 +0200

    components event detail

--- pages/events/[id]/index.js
+++ pages/events/[id]/index.js
@@ -1,7 +1,30 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../../dummy-data";
+import EventSummary from "../../../components/event-detail/event-summary";
+import EventContent from "../../../components/event-detail/event-content";
+import EventLogistics from "../../../components/event-detail/event-logistics";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const event = getEventById(id);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail id</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
